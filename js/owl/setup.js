79a2afaa5055dabe59d8bed5132cd32e893a5618
Lucas Cassiano <55335060+lucasoliverLO@users.noreply.github.com>
2021-10-07 16:15:49 -0300

    Adiciona layout da interface

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-carousel').css(
+    'background-color','#f4f6f8'
+);
